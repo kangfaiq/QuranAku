4668e060854512061f5de06b746457d2a448208a
Faiq Nukha <faiqnukha@gmail.com>
2020-08-25 20:35:49 +0700

    Initial commit

---
+++ app/src/main/java/net/fortinity/quranaku/model/ModelAyat.kt
@@ -0,0 +1,8 @@
+package net.fortinity.quranaku.model
+
+class ModelAyat {
+    var arab: String? = null
+    var indo: String? = null
+    var terjemahan: String? = null
+    var nomor: String? = null
+}
---
+++ app/src/main/java/net/fortinity/quranaku/networking/Api.java
@@ -0,0 +1,7 @@
+package net.fortinity.quranaku.networking;
+
+public class Api {
+    //API by BACHORS
+    public static String URL_LIST_SURAH = "https://api.npoint.io/99c279bb173a6e28359c/data";
+    public static String URL_LIST_AYAT = "https://api.npoint.io/99c279bb173a6e28359c/surat/{nomor}";
+}
